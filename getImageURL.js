b9dc4277825e5b4eecb16e33383828ccfb8483a2
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-12-17 11:44:05 -0300

    update getUrlImage script

--- getImageURL.js
+++ getImageURL.js
@@ -1,43 +1,41 @@
-const fs = require('fs')
-const { createCanvas, loadImage } = require('canvas')
+const fs = require("fs");
+const { createCanvas, loadImage } = require("canvas");
 
-const bg = "./assets/WhatsApp Image 2021-10-17 at 14.21.06.jpeg"
-
+const bg = process.argv[2];
 const sizes = {
-  mobile:{
-    width : 600,
-    height : 400    
+  mobile: {
+    width: 600,
+    height: 400,
   },
-  desktop:{
-    width : 1200,
-    height : 800    
+  desktop: {
+    width: 1200,
+    height: 800,
   },
-  devto:{
+  devto: {
     width: 1000,
-    height: 420
-  }
-}
+    height: 420,
+  },
+};
 
-function createImage(){
-  const { width } = sizes.mobile
-  const { height }= sizes.mobile
+function createImage() {
+  const { width } = sizes.desktop;
+  const { height } = sizes.desktop;
 
-  //Cria o canvas e o context que serão utilizados como área 
-  const canvas = createCanvas(width, height)
-  const context = canvas.getContext('2d')
-  
+  //Cria o canvas e o context que serão utilizados como área
+  const canvas = createCanvas(width, height);
+  const context = canvas.getContext("2d");
+
   //preenche a área com um quadro preto
-  context.fillStyle = 'white'
-  context.fillRect(0, 0, width, height)
-  
-  //Carrega o background
-  loadImage(bg).then(image => {
-    context.drawImage(image,  0, 0, width , height)
-    //Salva o arquivo
-    const dataUrl = canvas.toDataURL('image/png')
-    console.log(dataUrl.trimEnd())
-  });
+  context.fillStyle = "white";
+  context.fillRect(0, 0, width, height);
 
+  //Carrega o background
+  loadImage(bg).then((image) => {
+    context.drawImage(image, 0, 0, width, height);
+    //Salva o arquivo
+    const dataUrl = canvas.toDataURL("image/png");
+    console.log(dataUrl.trimEnd());
+  });
 }
 
 createImage();
